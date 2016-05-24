cadafbee982488bbaf1684ccf6794267d0a0fb23
PUR3-Mannerheim <tksotka@gmail.com>
2016-05-24 23:44:39 +0300

    Uusi testi

---
+++ primary.php
@@ -0,0 +1,36 @@
+<?xml version="1.0"?> 
+ 
+<primary.xml>
+		<Author>LTG_Mannerheim</Author>
+		<Author_Color>FF0000</Author_Color>
+		<Show_Author> yes </Show_Author>
+
+		<Check_Message_Time> 1 </Check_Message_Time>
+		
+		<MOTDmsg>PUR3 In-game tactics TEST</MOTDmsg>
+		<MOTD_Color>00FF00</MOTD_Color>
+		<Show_MOTD_Message> yes </Show_MOTD_Message>
+		
+		<Clan_Meeting>TEST</Clan_Meeting>
+		<Meeting_Color>FFCC00</Meeting_Color>
+		<Show_Meeting_Message> yes </Show_Meeting_Message>
+     
+		<Tournament_Battles_Message>TEST2</Tournament_Battles_Message> 
+		<Tournament_Color>FFCC00</Tournament_Color>
+		<Show_Tourney_Message> yes </Show_Tourney_Message>
+		
+     
+		<Clan_Wars_Message>TEST3</Clan_Wars_Message>
+		<CW_Color>FFCC00</CW_Color>
+		<Show_Clanwars_Message> yes </Show_Clanwars_Message>
+
+
+		<Show_Secondary_CW> yes </Show_Secondary_CW>
+		<Show_Both_CWMessages> yes </Show_Both_CWMessages>
+
+		<Enable_Secondary_Tourney>yes</Enable_Secondary_Tourney>
+		<Show_Both_TMessages>yes</Show_Both_TMessages>
+    
+		<Update_Mod_Config>no</Update_Mod_Config>
+		<Config_Update_Address>none</Config_Update_Address>
+</primary.xml>
